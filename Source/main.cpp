2a11d8ea257ac336b73a587d05f987bf355b368b
gost1212 <saadalgamdi888@gmail.com>
2024-02-09 03:48:49 +0300

    fix: fixed seg fault while getting file size. to think taking microsoft advice to use "GetFileSizeEx" instead of "GetFileSize" would make this problematic :(.

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,8 +5,8 @@
 /// @brief This function takes in {char*} that has the target file path and returns Heap HANDLE with the file data.
 /// @param char*_filePath file path in {char*}
 /// @return HANDLE: Returns an address in memory that holds the file content.
-HANDLE GetFileContent(const char* filePath){
-    
+HANDLE GetFileContent(const char* lpFilePath){
+
     //Windows API function:
     //param[1]: Takes a {char*} which has a value of file path.
     //param[2]: Specifies read/write access type requested, can be GENERIC_READ, GENERIC_WRITE, or both by usin " | " in between. or supply 0 for neither.
@@ -15,17 +15,17 @@ HANDLE GetFileContent(const char* filePath){
     //param[5]: Specifies the action to make on target file, you can pass CREATE_ALWAYS, CREATE_NEW, OPEN_ALWAYS, OPEN_EXISTING, TRUNCATE_EXISTING as you please.
     //param[6]: Specifies the file attributes when searching for the target file (aka. hidden, system, temporary, etc).
     //param[7]: A template of file attributes that can be used when (if) creating a new file.
-    const HANDLE hFile = CreateFileA(filePath, GENERIC_READ, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+    const HANDLE hFile = CreateFileA(lpFilePath, GENERIC_READ, 0, nullptr, OPEN_EXISTING, 0, nullptr);
     if(hFile == INVALID_HANDLE_VALUE){
         printf("An error occured while opening a file.\n");
         CloseHandle(hFile);
         return nullptr;
     }
-    
+
     //Windows API function:
     //param[1]: Takes a file handle to read it's size.
     //param[2]: Takes a {LARGE_INTEGER} structure to save a high-order doubleWord value. it's only required if you're trying to a file that's larger than 4Gb in size.
-    const DWORD_PTR dFileSize = GetFileSizeEx(hFile, nullptr);
+    const DWORD_PTR dFileSize = GetFileSize(hFile, nullptr);
     if(dFileSize == INVALID_FILE_SIZE){
         printf("An error occured when trying to get file size.\n");
         CloseHandle(hFile);
@@ -67,15 +67,20 @@ HANDLE GetFileContent(const char* filePath){
 
 int main(const int argc, char* argv[]){
     
-    char* filePath;//prepare pointer to save file path
+    char* lpFilePath;//prepare pointer to save file path
 
-    if(argc == 0){//check if there's any passed arguments
+    if(argc < 2){//check if there's any passed arguments
         printf("Please pass some arguments\n");
+        printf(argv[0]);
         return -1;
     }
-    filePath = argv[1];//take in passed file length
+    lpFilePath = argv[1];//take in passed file length
+    printf(argv[0]);
+    printf("\n");
+    printf(argv[1]);
+    printf("\n");
     
-    const HANDLE hFileContent = GetFileContent(filePath);
+    const HANDLE hFileContent = GetFileContent(lpFilePath);
     if(hFileContent == INVALID_HANDLE_VALUE){//check if HANDLE is invalid
         if(hFileContent != nullptr){//if so, check if it's not a null pointer
             CloseHandle(hFileContent);//close handle
